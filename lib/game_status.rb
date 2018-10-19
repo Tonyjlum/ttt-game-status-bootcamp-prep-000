bfa9191ae6728b740adbe24448cec5ca7895d73a
Tony <tonyjlum@gmail.com>
2018-10-19 01:22:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,5 +33,10 @@ def winner(board)
   symbol = won?(board)
   return nil unless symbol
   board[symbol.first]
+=======
+  return false if board.all?{|el| el == " "}
 
+  WIN_COMBINATIONS.find { |set| set.all? {|el| el == "X"}}
+
+>>>>>>> b586d6d8416c86fd746464c7c802bb8ad47da10a
 end
